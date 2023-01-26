f46ae3968e80736f03442fc1dc03618717432280
anniejoel <annhayriyan@gmail.com>
2023-01-26 15:52:14 +0400

    added the trimmer, it's full of bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Wrapper from "./Components/wrapper/wrapper";
 import {useEffect, useState} from "react";
+import Trimmer from "./Components/trimmer/trimmer";
 
 
 function App() {
 
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(null)
     const [browser, setBrowser] = useState("")
 
-    const handleOpen = () => {
-        setOpen(!open)
+    const handleOpen = (item) => {
+        setOpen(item)
     }
 
     useEffect(() => {
@@ -21,13 +22,26 @@ function App() {
         <div className="app">
             {browser.brand === "Google Chrome" && browser.version === "109"
                 ? (!open ?
-                    <button
-                        className="opener"
-                        onClick={handleOpen}>
-                        Open the List
-                    </button>
-                    : <Wrapper open={open} handleOpen={handleOpen}/>)
-                : <div className="error"> Please use Google Chrome's latest version to open correctly the app. </div>
+                        <div className="container">
+                            <button
+                                className="opener"
+                                onClick={handleOpen.bind(this, "list")}>
+                                Open the List
+                            </button>
+                            <button
+                                className="opener"
+                                onClick={handleOpen.bind(this, "trimmer")}>
+                                Open trimmer
+                            </button>
+                        </div>
+                        : open === "list" ?
+                            <Wrapper handleOpen={handleOpen.bind(this, null)}/>
+                            : <Trimmer/>
+                )
+                : <div className="error">
+                    Please use Google Chrome's latest version
+                    to open correctly the app.
+                </div>
             }
         </div>
     )
--- src/Components/wrapper/wrapper.jsx
+++ src/Components/wrapper/wrapper.jsx
@@ -3,27 +3,34 @@ import "./wrapper.css"
 import Column from "../column/column";
 
 function Wrapper({handleOpen}) {
-    return (
-        <section className="main-container">
-            <div className="header">
-                <h2>
-                    Should I eat at Black Angus?
-                </h2>
-                <span className="closer" onClick={handleOpen}>
-                    x
-                </span>
-            </div>
-            <div className="content">
-                <Column
-                    header={"Pros"}
-                />
-                <Column
-                    header={"Cons"}
-                />
 
-            </div>
-        </section>
+    return (
+        <div className="wrapper">
+            <section className="list-container">
+                <div className="header">
+                    <h2>
+                        Should I eat at Black Angus?
+                    </h2>
+                    <span className="closer" onClick={handleOpen}>
+                        x
+                    </span>
+                </div>
+                <div className="content">
+                    <Column
+                        header={"Pros"}
+                    />
+                    <Column
+                        header={"Cons"}
+                    />
+                </div>
+            </section>
+        </div>
+
+
     );
 }
 
 export default Wrapper;
+
+
+
