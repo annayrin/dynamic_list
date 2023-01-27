cc967a999623ff610577716bb17f4f84cb8973d1
anniejoel <annhayriyan@gmail.com>
2023-01-27 19:12:16 +0400

    changed all the trimmer logic, but the left resizer is not working

--- src/Components/resizable-div/resizable-div.jsx
+++ src/Components/resizable-div/resizable-div.jsx
@@ -54,10 +54,12 @@ function ResizableDiv() {
                     setCustomWidth(width)
                 }
             } else if (side === "left") {
-                setCustomLeft(event.clientX - initialLeft)
-                let width = customWidth - customLeft
-                // console.log(customWidth, width, customLeft)
-                setCustomWidth(width) // !!!!!!!!!!!!not working
+                setCustomLeft(event.clientX - initialLeft  - 7) // can't get why the offsetLeft is 7 px less
+                setCustomWidth(customWidth - (event.clientX - initialLeft - 7)) // !!!! not working
+
+
+
+
             } else {
                 return null
             }
@@ -93,11 +95,9 @@ function ResizableDiv() {
     return (
         <section className="trim-content-wrapper"
                  onMouseUp={handleMouseUp}
-
         >
             <div className="trim-container"
                  onMouseMove={handleMouseMove}
-
             >
                 <div
                     ref={divRef}
@@ -113,17 +113,23 @@ function ResizableDiv() {
                     <div className="drag"
                          onMouseDown={handleDrag}
                          onMouseUp={() => setDragging(false)}
-                    >
-                    </div>
-                    <div className='resizer right'
-                         onMouseDown={(event) => handleMouseDown(event, "right")}
-                    >
-                    </div>
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path
+                                d="M8.33 17.18a1.17 1.17 0 01-.25-1.25 1.14 1.14 0 011-.72h5.78a1.14 1.14 0 011 .72 1.17 1.17 0 01-.25 1.25l-2.88 3a1.09 1.09 0 01-1.58 0zm0-10.36l2.88-3a1.09 1.09 0 011.58 0l2.88 3a1.17 1.17 0 01.25 1.25 1.14 1.14 0 01-1 .72H9.11a1.14 1.14 0 01-1-.72 1.17 1.17 0 01.22-1.25z">
+                            </path>
+                        </svg>
+                </div>
+                <div className='resizer right'
+                     onMouseDown={(event) => handleMouseDown(event, "right")}
+                >
                 </div>
             </div>
-        </section>
+        </div>
+</section>
 
-    );
+)
+    ;
 }
 
 export default ResizableDiv
